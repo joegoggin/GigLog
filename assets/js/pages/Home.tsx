de82991d7b02b73b9aab01fff67fa554e0a90b7e
joegoggin <joegoggin27@gmail.com>
2025-11-08 16:22:57 -0500

    Update Home Page

--- assets/js/pages/Home.tsx
+++ assets/js/pages/Home.tsx
@@ -4,11 +4,18 @@ import MainLayout from "@/layouts/MainLayout";
 const HomePage: React.FC = () => {
     return (
         <MainLayout className="home-page" title="Phoenix Inertia Template">
-            <h1>Phoenix Inertia Auth Template</h1>
-            <div className="home-page__images">
-                <img src="/images/phoenix.png" alt="phoenix logo" />
-                <img src="/images/inertia.png" alt="inertia.js logo" />
-                <img src="/images/react.png" alt="react logo" />
+            <h1>GigLog</h1>
+            <div className="home-page__text">
+                <p>
+                    Freelancing opens a whole new world of opportunities giving
+                    you the freedom to work on your own terms. This freedom
+                    comes at a cost though. As a freelancer you are expected to
+                    keep track of your own hours, payments, taxes, and expenses.
+                    This can be hard to keep track of especially when you are
+                    working multiple different gigs as many freelancers do. Our
+                    goal at GigLog is to create a robust platform that has
+                    everything you need to keep track of in one place.
+                </p>
             </div>
             <div className="home-page__buttons">
                 <Button href={"/auth/sign-up"}>Sign Up</Button>
--- lib/app_web/controllers/public_controller.ex
+++ lib/app_web/controllers/public_controller.ex
@@ -1,18 +1,8 @@
 defmodule AppWeb.PublicController do
   use AppWeb, :controller
 
-  alias App.Notifications.Notification
-
   def home(conn, _params) do
-    notifications = [
-      Notification.new(:info, "Info", "This is an info message."),
-      Notification.new(:warning, "Warning", "This is a warning message."),
-      Notification.new(:success, "Success", "This is a success message."),
-      Notification.new(:error, "Error", "This is an error message.")
-    ]
-
     conn
-    |> put_flash(:notifications, notifications)
     |> render_inertia("Home")
   end
 end
